914d674d96ec4a3f62d8cf30345e17b3f629e896
nunes36 <theuswork@outlook.com>
2024-03-01 16:07:34 -0300

    primeiro commit treinamento js

---
+++ js/calcula-imc.js
@@ -0,0 +1,69 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+    for(var i = 0; i < pacientes.length ; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = document.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoValido = validaPeso(peso);
+    var alturaValida = validaAltura(altura);
+
+    if(!pesoValido) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
+        }
+
+    if(!alturaValida) {
+        console.log("altura inválida!");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+        }
+
+    if(alturaValida && pesoValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso <= 1000){
+        
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >=0 && altura <=3.0){
+        return true;
+    }else{
+        return false;
+    }
+}
+function calculaImc(peso,altura){
+    var imc = 0;
+
+    imc = peso / (altura = altura);
+
+    return imc.toFixed(2);
+}
+
+
+
+
+
+
